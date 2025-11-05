4edef0eb163dd1742a429d21a603f92aefda3bd0
Guillermo Guerrero <guillermo@guerreroibarra.com>
2025-11-05 22:22:56 +0100

    Fixed tests.

--- spec/aws/active_job/sqs/job_runner_spec.rb
+++ spec/aws/active_job/sqs/job_runner_spec.rb
@@ -4,20 +4,23 @@ module Aws
   module ActiveJob
     module SQS
       describe JobRunner do
-        let(:job_data) { TestJob.new('a1', 'a2').serialize }
-        let(:body) { ActiveSupport::JSON.dump(job_data) }
-        # message is a reserved minitest name
-        let(:msg) { double(data: double(body: body)) }
+        let(:job_serialized) { TestJob.new('a1', 'a2').serialize }
+        let(:body) { ActiveSupport::JSON.dump(job_serialized) }
+        let(:message_id) { '12345' }
+        let(:job_data) { job_serialized.merge('provider_job_id' => message_id) }
+        let(:msg) do
+          instance_double(Aws::SQS::Message, data: double(body: body, message_id: message_id))
+        end
+        let(:instance) { described_class.new(msg) }
 
         it 'parses the job data' do
-          job_runner = JobRunner.new(msg)
-          expect(job_runner.instance_variable_get(:@job_data)).to eq job_data
+          expect(instance.instance_variable_get(:@job_data)).to eq(job_data)
         end
 
         describe '#run' do
           it 'calls Base.execute with the job data' do
-            expect(::ActiveJob::Base).to receive(:execute).with(job_data)
-            JobRunner.new(msg).run
+            expect(::ActiveJob::Base).to receive(:execute).with(job_data).and_call_original
+            instance.run
           end
         end
       end
